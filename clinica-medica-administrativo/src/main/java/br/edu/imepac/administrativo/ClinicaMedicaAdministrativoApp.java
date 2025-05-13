68636d49c66a8a873cb26c27619fc2c29a2368c6
midinatto <midinatto@gmail.com>
2025-05-12 21:36:24 -0300

    a

---
+++ clinica-medica-administrativo/src/main/java/br/edu/imepac/administrativo/ClinicaMedicaAdministrativoApp.java
@@ -0,0 +1,11 @@
+package br.edu.imepac.administrativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ClinicaMedicaAdministrativoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaMedicaAdministrativoApp.class, args);
+    }
+}
